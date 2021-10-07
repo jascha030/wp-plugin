7aad0bf291ade6aee76769a6bc3132dd844ba3aa
Jascha030 <contact@jaschavanaalst.nl>
2021-10-07 19:07:33 +0200

    Run formatter

--- src/Exception/Hookable/DoesNotImplementHookableInterfaceException.php
+++ src/Exception/Hookable/DoesNotImplementHookableInterfaceException.php
@@ -3,16 +3,19 @@
 namespace Jascha030\ComposerTemplate\Exception\Hookable;
 
 use Jascha030\ComposerTemplate\Hookable\HookableInterface;
-use Throwable;
 
-class DoesNotImplementHookableInterfaceException extends \RuntimeException
+final class DoesNotImplementHookableInterfaceException extends \RuntimeException
 {
     private const IMPLEMENTING_INTERFACE = HookableInterface::class;
 
-    private const MESSAGE_TEMPLATE = "Class \"%s\" does not implement \"%s\".";
+    private const MESSAGE_TEMPLATE = 'Class "%s" does not implement "%s".';
 
-    public function __construct(string $className)
+    public function __construct(string $className, int $code = 0, \Throwable $previous = null)
     {
-        parent::__construct(sprintf(self::MESSAGE_TEMPLATE, $className, self::IMPLEMENTING_INTERFACE));
+        parent::__construct(
+            sprintf(self::MESSAGE_TEMPLATE, $className, self::IMPLEMENTING_INTERFACE),
+            $code,
+            $previous
+        );
     }
 }
