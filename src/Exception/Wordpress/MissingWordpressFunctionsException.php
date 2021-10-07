df8176f6232234d2ab92bbe0b8991328cde0ac8e
Jascha030 <contact@jaschavanaalst.nl>
2021-10-07 19:07:42 +0200

    Add exception for missing wordpress functions

--- src/Exception/Wordpress/MissingWordpressFunctionsException.php
+++ src/Exception/Wordpress/MissingWordpressFunctionsException.php
@@ -2,7 +2,12 @@
 
 namespace Jascha030\ComposerTemplate\Exception\Wordpress;
 
-class MissingWordpressFunctionsException
+final class MissingWordpressFunctionsException extends \RuntimeException
 {
+    private const MESSAGE_TEMPLATE = 'Couldn\'t load function: "%s", make sure this is an active WordPress install.';
 
+    public function __construct(string $function, int $code = 0, \Throwable $previous = null)
+    {
+        parent::__construct(sprintf(self::MESSAGE_TEMPLATE, $function), $code, $previous);
+    }
 }
