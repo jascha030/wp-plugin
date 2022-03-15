3cf72212604628565721511d9e2e139dfcc8d2cd
Jascha030 <contact@jaschavanaalst.nl>
2022-03-15 13:38:39 +0100

    ✨ Create Plugin data class.

--- src/Plugin.php
+++ src/Plugin.php
@@ -2,7 +2,33 @@
 
 namespace Jascha030\ComposerTemplate;
 
+use Jascha030\ComposerTemplate\Container\HookableStoreInterface;
+use Jascha030\ComposerTemplate\Registry\PluginRegistry;
+
 class Plugin
 {
+    private string $file;
 
+    private HookableStoreInterface $hookableStore;
+
+    private PluginRegistry $registry;
+
+    public function __construct(string $file)
+    {
+        if (! file_exists($file)) {
+            throw new \RuntimeException("Invalid plugin file path: \"{$file}\".");
+        }
+
+        $this->file = $file;
+    }
+
+    public function getFile(): string
+    {
+        return $this->file;
+    }
+
+    public function getRootDirectory(): string
+    {
+        return \dirname($this->file);
+    }
 }
