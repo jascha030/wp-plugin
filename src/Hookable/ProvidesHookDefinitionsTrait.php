0d2cd0d7fe0a3bd1f29a35353fb47fd7ad516963
Jascha030 <contact@jaschavanaalst.nl>
2022-03-15 13:37:23 +0100

    💡 Fix docs

--- src/Hookable/ProvidesHookDefinitionsTrait.php
+++ src/Hookable/ProvidesHookDefinitionsTrait.php
@@ -3,12 +3,14 @@
 namespace Jascha030\ComposerTemplate\Hookable;
 
 /**
- * Solves HookabkleInterface implementation for any class without need to repeatedly re-write methods, or
+ * Solves HookableInterface implementation for any class without need to repeatedly re-write methods, or
  * sacrifice hierarchically flexibility, being bound to HookableInterface implementing ancestors.
+ *
  * In other words: any method of any class should be able to interface with WordPress' plugin API, without being
  * forced to always construct it, while it might not be used during a request's lifecycle.
- * @property $actions array
- * @property $filters array
+ *
+ * @property array $actions
+ * @property array $filters
  */
 trait ProvidesHookDefinitionsTrait
 {
